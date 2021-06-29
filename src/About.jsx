11f3fc3f5572a26cc74c35ad6f8e7a5aa0561da8
TERRYSZHOU <TERRYSZHOU@GMAIL.COM>
2021-06-29 16:34:58 -0500

    Extra styling

--- src/About.jsx
+++ src/About.jsx
@@ -2,11 +2,11 @@ export const About = (props) => {
     let aboutData = props.aboutArray.map((person, id) => {
         return (
             <div className="person-box">
-                <header>
+                <header className="person-header">
                     <img src={person.image} alt="headshot"/>
                     <h3>{person.name}</h3>
                 </header>
-                <footer>
+                <footer id="person-footer">
                     <p>{person.body}</p>
                 </footer>
             </div>
@@ -14,10 +14,14 @@ export const About = (props) => {
     })
     return (
         <>
-            <h1>Meet Room One</h1>
-            <div className="about-box">
-                {aboutData}
-            </div>
+            <header className="about-header">
+                <h1>Meet Room One</h1>
+            </header>
+            <body>
+                <div className="about-box">
+                    {aboutData}
+                </div>
+            </body>
         </>
 
     )
