4f2ca71203a34f630106f5fd82976ed2cda7aebe
rei-bp <reinard.bp@gmail.com>
2021-06-29 14:48:55 -0700

    style footer more

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,10 +1,10 @@
 export const Footer = () => {
     return (
         <div className="footer">
-            <div className="footer-link"><a href="#">Our Projects</a></div>
-            <div className="footer-link"><a href="#">Contact</a></div>
-            <div className="footer-link"><a href="#">FAQ</a></div>
-            <div className="footer-link"><a href="#">Socials</a></div>
+            <div className="footer-link"><a className="footer-link-a" href="#">Our Projects</a></div>
+            <div className="footer-link"><a className="footer-link-a" href="#">Contact</a></div>
+            <div className="footer-link"><a className="footer-link-a" href="#">FAQ</a></div>
+            <div className="footer-link"><a className="footer-link-a" href="#">Socials</a></div>
         </div>
     )
 }
